26f1fffaf3aa12ce1f3ce725e5bd04714fa934c8
Oleksandr Diudiun <a.diudiun@gmail.com>
2017-08-05 15:29:22 +0300

    first

---
+++ examples/route.php
@@ -0,0 +1,42 @@
+<?php
+declare (strict_types = 1);
+require_once __DIR__ . "/vendor/autoload.php";
+
+use memCrab\Exceptions\FileException;
+use memCrab\Exceptions\RoutingException;
+use memCrab\File\Yaml;
+use memCrab\Router\Router;
+
+try {
+
+	# Read routes from yaml
+	$Yaml = new Yaml();
+
+	$routes = $Yaml->load("../src/routs.example.yaml", null)->getContent();
+
+	# For enable cache You can use FileCache object as second parametr of
+	# $Yaml->load() function. Use memCrab\Cache library for it.
+	# Redis Cache: $FileCache = new RedisCache([Redis obj]);
+	# PHP file Cache: $FileCache = new PHPCache([PathToTMLFolder]);
+
+	# Initialize Router
+	$Router = new Router();
+	$Router->loadRoutes($routes);
+
+	# Routing
+	$Router->matchRoute("http://example.com/post/", "POST");
+	# Run your Controller|Service|Component
+	$ServiceName = $Router->getService();
+	$ActionName = $Router->getAction();
+	$Service = new $ServiceName();
+	$Response = $Service->$Action($Router->getParams());
+} catch (RoutingException $error) {
+	$Respose = new \Response();
+	$Respose->setErrorResponse($error);
+} catch (FileException $error) {
+	$Respose = new \Response();
+	$Respose->setErrorResponse($error);
+}
+
+$Respose->sendHeaders();
+$Respose->sendContent();
---
+++ src/Router.php
@@ -0,0 +1,95 @@
+<?php
+declare (strict_types = 1);
+namespace memCrab\Router;
+use memCrab\Exceptions\RoutingException;
+
+/**
+ *  Router for core project
+ *
+ *  @author Oleksandr Diudiun
+ */
+class Router {
+	private $routes;
+	private $params;
+	private $actionName;
+	private $serviceName;
+	private $errorMessage;
+	private $errorServiceName;
+
+	function __construct() {}
+
+	public function loadRoutes(array $routes) {
+		if (empty($routes)) {
+			throw new RoutingException(_("Empty routes"), 1);
+		}
+
+		$this->routes = $routes;
+	}
+
+	public function matchRoute(string $url, string $method): void{
+		$url = parse_url($url);
+		if (!isset($url['path']) || is_string($url['path']) === false) {
+			throw new RoutingException(_("Router can't parse request."), 400);
+		}
+
+		if (!is_array($this->routes)) {
+			throw new RoutingException(_("Can't find any routes rules."), 501);
+		}
+
+		foreach ($this->routes as $regExpString => $route) {
+			$routes = 0;
+			$result = preg_match("/^" . str_replace("/", "\/", $regExpString) . "$/u", $url['path'], $matches);
+			if ($result === 0) {
+				continue;
+			} elseif ($result === false) {
+				throw new RoutingException(_("Can't parse route RegExp.") . $regExpString, 501);
+			} elseif ($result === 1) {
+				if (isset($route[$method])) {
+					$this->serviceName = $route[$method][0];
+					$this->actionName = $route[$method][1];
+
+					$paramsCount = count($route[$method]) - 2;
+					if ($paramsCount > 0) {
+						for ($i = 0; $i < $paramsCount; $i++) {
+							$this->params[$route[$method][$i + 2]] = $matches[$i + 1];
+						}
+					} else {
+						$this->params = null;
+					}
+
+					$routes++;
+					break;
+				}
+			}
+		}
+
+		if ($routes === 0) {
+			throw new RoutingException(_("Not found"), 404);
+		}
+
+		if ($routes > 1) {
+			throw new RoutingException(_("Conflict. Multiple routes found."), 501);
+		}
+
+	}
+
+	public function getParams():  ? array{
+		return $this->params;
+	}
+
+	public function getService() : string {
+		return $this->serviceName;
+	}
+
+	public function getAction():  ? string {
+		return $this->actionName;
+	}
+
+	public function getErrorMessage() :  ? string {
+		return $this->errorMessage;
+	}
+
+	public function getErrorServiceName() : string {
+		return $this->errorServiceName;
+	}
+}
