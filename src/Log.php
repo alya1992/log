0e7943607e0e7f53f7022a500031c2cd04412c5f
Oleksandr Diudiun <a.diudiun@gmail.com>
2017-08-05 16:03:03 +0300

    composer package

---
+++ src/Log.php
@@ -0,0 +1,13 @@
+<?php declare (strict_types = 1);
+namespace memCrab\Log;
+
+/**
+ *  Log for core project
+ *
+ *  @author Oleksandr Diudiun
+ */
+class Log {
+
+	function __construct() {}
+
+}
---
+++ tests/LogTest.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+/**
+ *  Corresponding Class to test Router class
+ *
+ *  For each class in your library, there should be a corresponding Unit-Test for it
+ *  Unit-Tests should be as much as possible independent from other test going on.
+ *
+ *  @author Oleksandr Diudiun
+ */
+class LogTest extends \PHPUnit\Framework\TestCase {
+
+}
