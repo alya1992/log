bfe02837e6610978a87dc22e6a6b04b6e769f6e0
Oleksandr Diudiun <a.diudiun@gmail.com>
2017-08-05 16:41:28 +0300

    add Layer

--- src/Log.php
+++ src/Log.php
@@ -1,6 +1,9 @@
 <?php declare (strict_types = 1);
 namespace memCrab\Log;
 
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
 /**
  *  Log for core project
  *
@@ -8,6 +11,19 @@ namespace memCrab\Log;
  */
 class Log {
 
-	function __construct() {}
+	private static $instance;
 
+	function __construct($name) {
+		$logger = new Logger('name');
+
+		$logger->pushHandler(new RotatingFileHandler('logs/' . $name . '/' . $name . '.log'));
+	}
+
+	public static function stream($name) {
+		if (!isset(self::$instance[$name])) {
+			self::$instance[$name] = new self($name);
+		}
+
+		return self::$instance[$name];
+	}
 }
