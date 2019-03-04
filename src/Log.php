64419d96f99406795206305bdb384ff36ba8118a
alya1992 <37505269+alya1992@users.noreply.github.com>
2019-03-04 11:04:41 +0200

    add setSqsHandler

--- src/Log.php
+++ src/Log.php
@@ -2,7 +2,10 @@
 namespace memCrab\Log;
 
 use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\SqsHandler;
 use Monolog\Logger;
+use Aws\Sqs\SqsClient;
+
 
 /**
  *  Log for core project
@@ -25,5 +28,17 @@ class Log {
 
 	public static function setDefaultRotationHandler($name) {
 		self::stream($name)->pushHandler(new RotatingFileHandler('logs/' . $name . '/' . $name . '.log'));
+	}
+
+	public static function setSqsHandler(string $region, string $key, string $secret, string $version, $queueUrl, $name) {
+		$config = [
+                'region' => $region,
+                'version' => $version,
+                'credentials' => array(
+                    'key' => $key,
+                    'secret' => $secret,
+                )];
+		$sqsClient = new SqsClient($config);
+		$test = self::stream($name)->pushHandler(new SqsHandler($sqsClient, $queueUrl));
 	}
 }
