43fde6eecbe15b0bfe86c5e18d2ea31cdc2cf516
Oleksandr Diudiun <a.diudiun@gmail.com>
2019-03-06 16:24:33 +0200

    fix

--- src/Log.php
+++ src/Log.php
@@ -1,11 +1,10 @@
 <?php declare (strict_types = 1);
 namespace memCrab\Log;
 
+use Aws\Sqs\SqsClient;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Handler\SqsHandler;
 use Monolog\Logger;
-use Aws\Sqs\SqsClient;
-
 
 /**
  *  Log for core project
@@ -14,23 +13,23 @@ use Aws\Sqs\SqsClient;
  */
 class Log {
 
-	private static $instance;
+    private static $instance;
 
-	function __construct() {}
+    function __construct() {}
 
-	public static function stream($name) {
-		if (!isset(self::$instance[$name])) {
-			self::$instance[$name] = new Logger('name');
-		}
+    public static function stream($name) {
+        if (!isset(self::$instance[$name])) {
+            self::$instance[$name] = new Logger($name);
+        }
 
-		return self::$instance[$name];
-	}
+        return self::$instance[$name];
+    }
 
-	public static function setDefaultRotationHandler($name) {
-		self::stream($name)->pushHandler(new RotatingFileHandler('logs/' . $name . '/' . $name . '.log'));
-	}
+    public static function setDefaultRotationHandler($name) {
+        self::stream($name)->pushHandler(new RotatingFileHandler('logs/' . $name . '/' . $name . '.log'));
+    }
 
-	public static function setSqsHandler(SqsClient $sqsClient, $queueUrl, $name) {
-		self::stream($name)->pushHandler(new SqsHandler($sqsClient, $queueUrl));
-	}
+    public static function setSqsHandler(SqsClient $sqsClient, $queueUrl, $name) {
+        self::stream($name)->pushHandler(new SqsHandler($sqsClient, $queueUrl));
+    }
 }
